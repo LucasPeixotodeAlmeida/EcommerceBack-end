c3be8617ec9aacf77859931311b74ced88949df2
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-10-21 19:38:04 -0300

    Fix: alterando nome do path para adequar ao front

--- src/main/java/com/lucas/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/lucas/ecommerce/dao/ProductCategoryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-@RepositoryRestResource(collectionResourceRel = "products", path = "products")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 @CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategoryEntity, Long> {
 }
